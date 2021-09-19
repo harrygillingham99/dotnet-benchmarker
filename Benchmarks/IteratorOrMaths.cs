2b831d6bc565d6bdbe2842aecd7372db73acf779
harrygillingham99 <harrygillingham99@gmail.com>
2021-09-19 15:35:37 +0100

    feat(command-loop): add a basic extensible command loop and wire up some commands

--- Benchmarks/IteratorOrMaths.cs
+++ Benchmarks/IteratorOrMaths.cs
@@ -1,40 +1,30 @@
-﻿using Benchmarker.Benchmarks.Templates;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Benchmarker.Benchmarks.Templates;
 
 namespace Benchmarker.Benchmarks
 {
     public class IteratorOrMaths : SimpleComparisonBenchmark
     {
-        private static double LoopAmount
-        {
-            get
-            {
-                return 10 * Math.Pow(10, 5);
-            }
-        }
-
-        public IteratorOrMaths() : base("Iterator or summation", "Comparing iterating through a list and summing or a mathematical algorithm", 
+        public IteratorOrMaths() : base("Iterator or summation",
+            "Comparing iterating through a list and summing or a mathematical algorithm",
             new List<(string variantName, Action benchmarkDelegate)>
-        {
-            ("Iterator", () =>
             {
-                var sum = 0;
+                ("Iterator", () =>
+                {
+                    var sum = 0;
 
-            for (var i = 1; i <= LoopAmount; i++)
-            {
-                sum += i;
-            }
-            }),
-            ("Summation", () => 
-            {
-                var n = LoopAmount; // upper bound
-                var sumTwo = (n * (n + 1)) / 2;
+                    for (var i = 1; i <= LoopAmount; i++) sum += i;
+                }),
+                ("Summation", () =>
+                {
+                    var n = LoopAmount; // upper bound
+                    var sumTwo = n * (n + 1) / 2;
+                })
             })
-        })
         {
-
         }
+
+        private static double LoopAmount => 10 * Math.Pow(10, 5);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Benchmarker
 {
@@ -6,11 +8,76 @@ namespace Benchmarker
     {
         private static void Main(string[] args)
         {
-            new BenchmarkRunner.BenchmarkRunner().RunBenchmarks();
+            while (true)
+            {
+                Console.WriteLine("Enter next command: (type help to see all commands)");
+                ProcessCommand(Console.ReadLine());
+            }
+        }
 
-            Console.WriteLine("\n \n \n \nDone! Press any key to close...");
+        private static void ProcessCommand(string input)
+        {
+            var cleanupConsole = true;
+
+            bool InputMatchesCommand(Commands command)
+            {
+                return input.Equals(GetEnumDescription(command),
+                    StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            switch (input)
+            {
+                case var _ when InputMatchesCommand(Commands.AllTests):
+                    new BenchmarkRunner.BenchmarkRunner().RunBenchmarks();
+                    break;
+                case var _ when InputMatchesCommand(Commands.Help):
+                    PrintAllCommands();
+                    cleanupConsole = false;
+                    break;
+                case var _ when InputMatchesCommand(Commands.Quit):
+                    Environment.Exit(0);
+                    return;
+                default:
+                    Console.WriteLine("Command not found, type help to see all commands.");
+                    cleanupConsole = false;
+                    break;
+            }
+
+            if (cleanupConsole) CleanupConsole();
+        }
+
+        private static string GetEnumDescription(Enum value)
+        {
+            // Get the Description attribute value for the enum value
+            var fi = value.GetType().GetField(value.ToString());
+            var attributes = (DescriptionAttribute[]) fi?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes?.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+
+        private static void CleanupConsole()
+        {
+            Console.WriteLine("\n \nDone! Press any key...");
             Console.ReadKey();
             Console.Clear();
+        }
+
+        private static void PrintAllCommands()
+        {
+            var descriptions = Enum.GetValues(typeof(Commands)).Cast<Commands>()
+                .Where(command => command != Commands.Help)
+                .Select(command => GetEnumDescription(command));
+            Console.WriteLine("The following commands are not case sensitive: ");
+            descriptions.ToList().ForEach(Console.WriteLine);
+        }
+
+        internal enum Commands
+        {
+            [Description("Help")] Help = 0,
+            [Description("Run All Tests")] AllTests = 1,
+            [Description("List All Tests")] ListTests = 2,
+            [Description("Run Specific Test")] SpecificTest = 3,
+            [Description("Quit")] Quit = 4
         }
     }
 }
